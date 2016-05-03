49e1b79d59fd5c158cfc1d2be751bf7aa91e1968
CJ Jessett <cjessett@gmail.com>
2016-05-03 13:24:59 -0500

    Fix conditional into proper order

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,34 +1,45 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed?"
+employees = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp.to_i
+until employees == 0 
+	puts "What is your name?"
+	name = gets.chomp
 
-puts "What year were you born?"
-year_born = gets.chomp.to_i
+	puts "How old are you?"
+	age = gets.chomp.to_i
 
-puts "Our cafeteria serves garlic bread, would you like some?(y/n)"
-gets.chomp == "y" ? garlic_bread = true : garlic_bread = false
+	puts "What year were you born?"
+	year_born = gets.chomp.to_i
 
-puts "Would you like to enroll in the company's health insurance?(y/n)"
-gets.chomp == "y" ? insurance = true : insurance = false
+	puts "Our cafeteria serves garlic bread, would you like some?(y/n)"
+	gets.chomp == "y" ? garlic_bread = true : garlic_bread = false
 
-probable_age = 2016 - year_born
+	puts "Would you like to enroll in the company's health insurance?(y/n)"
+	gets.chomp == "y" ? insurance = true : insurance = false
 
-if probable_age == age || probable_age == age + 1 # Here I account for someone who hasn't reached their birthday this year
-	correct_age = true
-else
-	correct_age = false
-end
+	probable_age = 2016 - year_born
 
-if correct_age && ( garlic_bread || insurance )
-	puts "Probably not a vampire."
-elsif !correct_age && ( !garlic_bread || !insurance )
-	puts "Probably a vampire."
-elsif !correct_age && ( !garlic_bread && !insurance )
-	puts "Almost certainly a vampire."
-elsif name == "Drake Cula" || name == "Tu Fang"
-	puts "Definitely a vampire."
-else
-	puts "Results inconclusive"
+	if probable_age == age || probable_age == age + 1 # Here I account for someone who hasn't reached their birthday this year
+		correct_age = true
+	else
+		correct_age = false
+	end
+
+	p correct_age
+	p garlic_bread
+	p insurance
+
+	if name == "Drake Cula" || name == "Tu Fang"
+		puts "Definitely a vampire."
+	elsif correct_age && ( garlic_bread || insurance )
+		puts "Probably not a vampire."
+	elsif !correct_age && (!garlic_bread && !insurance)
+		puts "Almost certainly a vampire."
+	elsif !correct_age && ( !garlic_bread || !insurance )
+		puts "Probably a vampire."
+	else
+		puts "Results inconclusive"
+	end
+
+	employees -= 1
 end
