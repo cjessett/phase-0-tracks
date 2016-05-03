19ce938cc3fe71424820eb785e8607894ece4aad
CJ Jessett <cjessett@gmail.com>
2016-05-03 12:41:58 -0500

    Add prompt

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+year_born = gets.chomp.to_i
+
+puts "Our cafeteria serves garlic bread, would you like some?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
+
