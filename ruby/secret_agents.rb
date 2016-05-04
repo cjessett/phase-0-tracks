da6f980963a488cb900101d9c072687cc5c7ba64
CJ Jessett <cjessett@gmail.com>
2016-05-03 21:26:42 -0500

    Add user interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -10,11 +10,13 @@ def encrypt(string)
 	index = 0
 	while index < string.length
 		if string[index] == " "
-			index += 1
+
+		elsif string[index] == "z"
+			string[index] = "a"
 		else
 			string[index] = string[index].next
-			index += 1
 		end
+		index += 1
 	end
 	return string
 end
@@ -35,13 +37,41 @@ def decrypt(string)
 
 	while index < string.length
 		if string[index] == " "
-			index += 1
+
 		else
 			i = alphabet.index(string[index])
 			string[index] = alphabet[i - 1]
-			index += 1
 		end
+		index += 1
 	end
 	return string
 end
 
+=begin
+
+puts encrypt("abc") == "bcd"
+puts encrypt("zed") == "afe"
+puts decrypt("bcd") == "abc"
+puts decrypt("afe") == "zed" 
+
+puts decrypt(encrypt("swordfish"))
+#Because encrypt returns a string and decrypt receives a string as an argument, this nested method call works.
+=end
+
+# Prompts user for encryption or decryption
+# Asks user for password
+# prints the requested operations
+
+puts "Would you like to encrypt (e) or decrypt (d)?:"
+operation = gets.chomp
+
+puts "Please enter password:"
+password = gets.chomp.downcase
+
+if operation == "e"
+	p encrypt(password)
+elsif operation == "d"
+	p decrypt(password)
+else
+	puts "Sorry, I did not get that."
+end
