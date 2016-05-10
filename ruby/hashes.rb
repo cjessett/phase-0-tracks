47ca59fb7f54580645589cea3c8ea77b5944df4f
CJ Jessett <cjessett@gmail.com>
2016-05-10 12:41:45 -0500

    Add control flow and conditional

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -8,9 +8,9 @@
 # number of children
 # decor theme
 
-# update a key/value pair or none
+# update a key/value pair if necessary
 
-clients = Hash.new("Unknown")
+clients = Hash.new
  
 puts "What is your client's name: "
 clients[:name] = gets.chomp
@@ -21,4 +21,18 @@ clients[:age] = gets.chomp.to_i
 puts "How many children does your client have: "
 clients[:children] = gets.chomp.to_i
 
-puts ""
+puts "What is your client's decor theme: "
+clients [:decor_theme] = gets.chomp
+
+p clients
+
+puts "Enter a key you would like to modify or type none: "
+choice = gets.chomp
+
+if choice = "none"
+	break
+else puts "What is the new value for #{choice.to_sym}: "
+	clients[choice.to_sym] = gets.chomp
+end
+
+p clients
