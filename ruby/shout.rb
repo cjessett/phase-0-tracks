142a53abd0bb7ad31ca79355ba859f097356993f
CJ Jessett <cjessett@gmail.com>
2016-05-16 17:49:26 -0500

    Convert Shout module to mixin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 	def self.yell_angrily(words)
 		words + "!!!" + " :("
@@ -10,3 +11,31 @@ end
 
 Shout.yell_angrily("Arghh")
 Shout.yelling_happily("Yippy")
+=end
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yelling_happily(words)
+		words + "!!!" + " :)"
+	end
+end
+
+class Human
+	include Shout
+end
+
+class Goat
+	include Shout
+end
+
+human = Human.new
+puts human.yell_angrily("arghh")
+puts human.yelling_happily("Yippy")
+
+goat = Goat.new
+puts goat.yell_angrily("baaahhhh")
+puts goat.yelling_happily("baaahhhh")
+
