87569c60f6ae0fcd2a09754a55878c6952d8ef2b
CJ Jessett <cjessett@gmail.com>
2016-05-16 14:38:13 -0500

    Add getter and setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,10 +16,40 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
-	def about
-		puts "gender: #{@gender}"
-		puts "ethnicity: #{@ethnicity}"
+	# setter methods
+	def celebrate_birthday
+		@age += 1
+	end
+
+	def get_mad_at(reindeer)
+		p @reindeer_ranking
+		reindeer_idx = @reindeer_ranking.index(reindeer)
+		@reindeer_ranking.insert(-1, @reindeer_ranking.delete_at(reindeer_idx))
+	end
+
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+
+	# getter methods
+	def age
+		@age
+	end
+
+	def ethnicity
+		@ethnicity
 	end
 
 end
 
+saint_nick = Santa.new("male", "white")
+
+puts saint_nick.celebrate_birthday
+
+puts saint_nick.get_mad_at("Vixen")
+
+puts saint_nick.gender = "bisexual"
+
+puts saint_nick.age
+
+puts saint_nick.ethnicity
