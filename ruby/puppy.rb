aaf5492b5f2c415592b9c8d06e1ad7633ee05f32
CJ Jessett <cjessett@gmail.com>
2016-05-16 12:26:02 -0500

    Add puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+=begin
+
+CLASS ------------------
+Puppy
+
+CHARACTERISTICS --------
+Friendly: yes
+Leg count: 2
+Name: varies
+Color: varies
+Size: varies
+
+BEHAVIOR ---------------
+Bark
+Wag tail
+Run
+
+=end
