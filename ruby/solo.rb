d0a0e7f290d4a3e5df317921fd20c56c17f4fbfc
CJ Jessett <cjessett@gmail.com>
2016-05-16 16:04:21 -0500

    Add pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,8 @@
+# Build a class with:
+
+# at least three attributes (using at least two data types) 
+# whose values will vary from instance to instance (example: age)
+
+# at least three methods, at least one of which takes an argument
+
+# define 
