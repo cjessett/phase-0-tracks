e56d4ce5b28a7b824af20482f211e2fae31b35d3
CJ Jessett <cjessett@gmail.com>
2016-05-17 07:28:06 -0500

    Add goat class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -5,4 +5,31 @@
 
 # at least three methods, at least one of which takes an argument
 
-# define 
+# define class goats
+
+# attributes: attitude, sex, name (accessor)
+# methods: milk, speak(# of times), feed -- changes attitude
+# attitude_array = ["stoked", "fiesty", "mean", "goofy"]
+
+class Goat
+	attr_accessor :name
+	attr_reader :attitude, :sex
+
+	
+	@attitude = attitude.sample
+
+	def initialize(name, sex)
+		puts "Initializing goat instance..."
+	end
+
+	def milk
+		puts "Baaahhhh  **milking**"
+	end
+
+	def speak(num)
+		num.times { puts "Baaahhh" }
+	end
+
+	def 
+
+end
